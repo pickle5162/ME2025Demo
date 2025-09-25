3ea4b1596a24aa029652ddbfd128c3193c0710d8
Ian <ianwinwinwin@gmail.com>
2025-09-25 14:59:29 +0800

    第一個作業完成了可喜可賀

---
+++ HW4_JavaScript1/static/114303031_js_1.js
@@ -0,0 +1,18 @@
+let counter=0
+let answernum = Math.floor(Math.random()*101);
+function checkguess(){
+    let guessnum= document.getElementById("number").value;
+    counter++;
+    if(guessnum>answernum){
+        alert("too big");
+    }
+    if(guessnum<answernum){
+        alert("too small");
+    }
+    if(guessnum==answernum){
+        alert("You are right. Guess times="+counter)
+        counter=0;
+        answernum = Math.floor(Math.random()*101);
+    }
+}
+
