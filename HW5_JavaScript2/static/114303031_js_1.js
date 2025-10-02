79363c0432eab497518073ea37886f71b6911c30
Ian <ianwinwinwin@gmail.com>
2025-10-02 14:53:53 +0800

    試做timer但一直錯不做了先做別的

--- HW5_JavaScript2/static/114303031_js_1.js
+++ HW5_JavaScript2/static/114303031_js_1.js
@@ -1,6 +1,15 @@
-let counter=0
+let counter=0;
+let timer=0;
+let timersum=0;
+let timerid=0;
 let answernum = Math.floor(Math.random()*101);
 function checkguess(){
+    timersum+=timer;
+    timer=0;
+    clearInterval(timerid);
+    timerid=setInterval(()=>{
+        timer++;
+    },10)
     let guessnum= document.getElementById("number").value;
     counter++;
     if(guessnum>answernum){
@@ -11,7 +20,12 @@ function checkguess(){
     }
     if(guessnum==answernum){
         alert("You are right. Guess times="+counter);
+        timersum+=timer;
+        alert(timersum/100.0);
         counter=0;
+        timer=0;
+        timersum=0;
         answernum = Math.floor(Math.random()*101);
     }
+    
 }
