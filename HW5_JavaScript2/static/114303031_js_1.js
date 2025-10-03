d8fc0a63989753e9f157b51e56550ab338155ce6
Ian <ianwinwinwin@gmail.com>
2025-10-03 22:08:07 +0800

    加入enter輸入法跟計時器

--- HW5_JavaScript2/static/114303031_js_1.js
+++ HW5_JavaScript2/static/114303031_js_1.js
@@ -1,34 +1,45 @@
 let counter=0;
-let timer=0;
 let timersum=0;
+let timer=0;
 let timerid=0;
+let timecounter=1;
 let answernum = Math.floor(Math.random()*101);
+let timerdisplay = document.getElementById("timerdisplay");
 let container = document.getElementById("container");
+let hint = document.getElementById("hint");
+document.getElementById("number").addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        checkguess();
+    }
+});
 function checkguess(){
-    let newParagraph = document.createElement("p");
-    timersum+=timer;
-    timer=0;
-    clearInterval(timerid);
-    timerid=setInterval(()=>{
-        timer++;
-    },10)
+    if(counter===0){
+        clearInterval(timerid);
+        timerid=setInterval(()=>{
+            timer+=10;
+            timerdisplay.textContent="用時"+(timer/1000.0)+"秒"
+        },10)
+    }
     let guessnum= document.getElementById("number").value;
     counter++;
     if(guessnum>answernum){
-        container.textContent = "太大了";
+        hint.textContent = "太大了";
     }
     if(guessnum<answernum){
-        container.textContent = "太小了";
+        hint.textContent = "太小了";
     }
     if(guessnum==answernum){
+        let newParagraph = document.createElement("p");
+        container.appendChild(newParagraph);
+        clearInterval(timerid);
+        newParagraph.textContent =timecounter+". 猜了"+counter+"次,耗時"+(timer/1000.0)+"秒";
+        
         alert("You are right. Guess times="+counter);
         timersum+=timer;
-        newParagraph.textContent = "猜了"+counter+"次,耗時秒";
-        alert(timersum/100.0);
+        
         counter=0;
-        timer=0;
-        timersum=0;
         answernum = Math.floor(Math.random()*101);
+        timecounter++;
     }
-    container.appendChild(newParagraph);
+    
 }
