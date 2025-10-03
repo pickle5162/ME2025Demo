37754b47c81799e7edda5752e15743a022517f11
Ian <ianwinwinwin@gmail.com>
2025-10-03 20:42:48 +0800

    提示顯示在畫面上了

--- HW5_JavaScript2/static/114303031_js_1.js
+++ HW5_JavaScript2/static/114303031_js_1.js
@@ -3,7 +3,9 @@ let timer=0;
 let timersum=0;
 let timerid=0;
 let answernum = Math.floor(Math.random()*101);
+let container = document.getElementById("container");
 function checkguess(){
+    let newParagraph = document.createElement("p");
     timersum+=timer;
     timer=0;
     clearInterval(timerid);
@@ -13,19 +15,20 @@ function checkguess(){
     let guessnum= document.getElementById("number").value;
     counter++;
     if(guessnum>answernum){
-        alert("too big");
+        container.textContent = "太大了";
     }
     if(guessnum<answernum){
-        alert("too small");
+        container.textContent = "太小了";
     }
     if(guessnum==answernum){
         alert("You are right. Guess times="+counter);
         timersum+=timer;
+        newParagraph.textContent = "猜了"+counter+"次,耗時秒";
         alert(timersum/100.0);
         counter=0;
         timer=0;
         timersum=0;
         answernum = Math.floor(Math.random()*101);
     }
-    
+    container.appendChild(newParagraph);
 }
