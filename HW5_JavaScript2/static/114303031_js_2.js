0e558fbfbf4c5cba40f933dba84051befd7fb6b0
Ian <ianwinwinwin@gmail.com>
2025-10-04 22:44:49 +0800

    記數量的格子跟按鈕做好了

--- HW5_JavaScript2/static/114303031_js_2.js
+++ HW5_JavaScript2/static/114303031_js_2.js
@@ -1,5 +1,16 @@
 const checkboxAll = document.getElementById("checkbox_all");
 const checkboxItems = document.querySelectorAll(".checkbox_item");
+let stock1=document.getElementById("stock1")
+let stock2=document.getElementById("stock2")
+let stock3=document.getElementById("stock3")
+function increase(id) {
+  const input = document.getElementById(id);
+  input.value = Number(input.value) + 1;
+} 
+function decrease(id) {
+  const input = document.getElementById(id);
+  if (input.value > 1) input.value = Number(input.value) - 1;
+}
 checkboxAll.addEventListener("change", () => {
     checkboxItems.forEach(item => item.checked = checkboxAll.checked);
 });
@@ -9,3 +20,6 @@ checkboxItems.forEach(item => {
         checkboxAll.checked = allChecked;
     });
 });
+stock1.textContent="50";
+stock2.textContent="100";
+stock3.textContent="150";
