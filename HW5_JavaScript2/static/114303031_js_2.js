dfe0841656566c0d0a33a31f0b832311c227446c
Ian <ianwinwinwin@gmail.com>
2025-10-05 00:29:59 +0800

    修正商品庫存單價數量總計的bug

--- HW5_JavaScript2/static/114303031_js_2.js
+++ HW5_JavaScript2/static/114303031_js_2.js
@@ -1,29 +1,80 @@
 const checkboxAll = document.getElementById("checkbox_all");
 const checkboxItems = document.querySelectorAll(".checkbox_item");
-let stock1=document.getElementById("stock1")
-let stock2=document.getElementById("stock2")
-let stock3=document.getElementById("stock3")
+let stock1=document.getElementById("stock1");
+let stock2=document.getElementById("stock2");
+let stock3=document.getElementById("stock3");
 stock1.textContent="49";
 stock2.textContent="99";
 stock3.textContent="149";
-function increase(quantityId, stockId) {
-    const input = document.getElementById(quantityId);
+const initialStocks = {
+    stock1: 49,
+    stock2: 99,
+    stock3: 149
+};
+cost1.textContent="500";
+cost2.textContent="1000";
+cost3.textContent="2000";
+function increase(quantityId,stockId,costId,unitPrice) {
+    const quantity = document.getElementById(quantityId);
     const stock = document.getElementById(stockId);
+    const cost = document.getElementById(costId);
     let stockValue = Number(stock.textContent);
+    let quantityValue = Number(quantity.value);
     if (stockValue > 0) {
-        input.value = Number(input.value) + 1;
-        stock.textContent = stockValue - 1;
+        quantity.value=quantityValue+1;
+        stock.textContent=stockValue-1;
+        cost.textContent=(quantityValue+1)*unitPrice;
   }
-} 
-function decrease(quantityId, stockId) {
-    const input = document.getElementById(quantityId);
+}
+function decrease(quantityId,stockId,costId,unitPrice) {
+    const quantity = document.getElementById(quantityId);
     const stock = document.getElementById(stockId);
+    const cost = document.getElementById(costId);
     let stockValue = Number(stock.textContent);
-    if (input.value > 1){
-        input.value = Number(input.value) - 1;
-        stock.textContent = stockValue + 1;
+    let quantityValue = Number(quantity.value);
+    if (quantity.value > 1){
+        quantity.value=quantityValue-1;
+        stock.textContent=stockValue+1;
+        cost.textContent=(quantityValue-1)*unitPrice;
     }
 }
+function changeQuantity(quantityId,stockId,costId,unitPrice){
+    const quantity = document.getElementById(quantityId);
+    const stock = document.getElementById(stockId);
+    const cost = document.getElementById(costId);
+    let stockValue = Number(stock.textContent);
+    let quantityValue=quantity.value;
+    if (quantityValue==="") {
+        stock.textContent=initialStocks[stockId];
+        return;
+    }
+    if (quantityValue>=1&&quantityValue<=initialStocks[stockId]) {
+        cost.textContent=quantityValue*unitPrice;
+        stock.textContent=initialStocks[stockId]-quantityValue;
+        return;
+    }
+    if (quantityValue>initialStocks[stockId]) {
+        quantity.value=initialStocks[stockId];
+        cost.textContent=(initialStocks[stockId]+1)*unitPrice;
+        stock.textContent="0";
+        return;
+    }
+    if (quantityValue<1) {
+        quantity.value=1;
+        cost.textContent=unitPrice;
+        return;
+    }
+    
+}
+function fixQuantity(quantityId, stockId, costId, unitPrice) {
+    const quantity = document.getElementById(quantityId);
+    if (quantity.value === "") {
+        quantity.value = 1;
+        stock.textContent=initialStocks[stockId];
+    }
+}
+
+
 checkboxAll.addEventListener("change", () => {
     checkboxItems.forEach(item => item.checked = checkboxAll.checked);
 });
