65e756db9a22a4cd2a8e957dee0cebc9fff7de5e
Ian <ianwinwinwin@gmail.com>
2025-10-04 23:02:36 +0800

    庫存跟數量的關係做好了

--- HW5_JavaScript2/static/114303031_js_2.js
+++ HW5_JavaScript2/static/114303031_js_2.js
@@ -3,13 +3,26 @@ const checkboxItems = document.querySelectorAll(".checkbox_item");
 let stock1=document.getElementById("stock1")
 let stock2=document.getElementById("stock2")
 let stock3=document.getElementById("stock3")
-function increase(id) {
-  const input = document.getElementById(id);
-  input.value = Number(input.value) + 1;
+stock1.textContent="49";
+stock2.textContent="99";
+stock3.textContent="149";
+function increase(quantityId, stockId) {
+    const input = document.getElementById(quantityId);
+    const stock = document.getElementById(stockId);
+    let stockValue = Number(stock.textContent);
+    if (stockValue > 0) {
+        input.value = Number(input.value) + 1;
+        stock.textContent = stockValue - 1;
+  }
 } 
-function decrease(id) {
-  const input = document.getElementById(id);
-  if (input.value > 1) input.value = Number(input.value) - 1;
+function decrease(quantityId, stockId) {
+    const input = document.getElementById(quantityId);
+    const stock = document.getElementById(stockId);
+    let stockValue = Number(stock.textContent);
+    if (input.value > 1){
+        input.value = Number(input.value) - 1;
+        stock.textContent = stockValue + 1;
+    }
 }
 checkboxAll.addEventListener("change", () => {
     checkboxItems.forEach(item => item.checked = checkboxAll.checked);
@@ -20,6 +33,3 @@ checkboxItems.forEach(item => {
         checkboxAll.checked = allChecked;
     });
 });
-stock1.textContent="50";
-stock2.textContent="100";
-stock3.textContent="150";
